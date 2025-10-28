75bfc15dfabc69bb5594387c737abeed23d2fcf5
shreyashmahalle <128777174+shreyashmahalle@users.noreply.github.com>
2025-10-28 10:29:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 window.promises = [];
-for(let i = 0; i < 5; i++){
-const time = Math.floor(Math.random() *5) +1;
-const Promise = new Promise((resolve) => {
-setTimeOut(() => {
-resolve(`Promise ${i + 1}resolved in ${time} seconds`);
-},time * 1000);
-});
-	window.Promises.push(Promise);
+
+for (let i = 0; i < 5; i++) {
+  const time = Math.floor(Math.random() * 5) + 1; 
+
+  const promise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(Promise ${i + 1} resolved in ${time} seconds);
+    }, time * 1000);
+  });
+
+  window.promises.push(promise);
 }
-Promise.any(window.Promise).then(result) =>{
-document.getElementById("output").innerText = result;
+
+Promise.any(window.promises).then((result) => {
+  document.getElementById("output").innerText = result;
 });
-// Do not change the code above this
-// add your promises to the array `promises`
